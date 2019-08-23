f021e6c1e227743ba5c6244009bf188498a3de5a
vishalicious213 <vishalicious213@gmail.com>
2019-08-23 11:42:41 -0400

    array 1 done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,7 +74,15 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+let count;
 const universities = [];
+
+for (count = 0; count <graduates.length; count++) {
+  universities.push(graduates[count].university);           // new array with universities
+}
+
+universities.sort();                                        // sort universities
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
